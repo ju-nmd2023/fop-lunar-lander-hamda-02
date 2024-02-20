f7852fa6adc3423a4e3d3521439cd6a9f3459151
hamda-02 <saha23hm@student.ju.se>
2024-02-20 23:37:38 +0100

    #4
    land on moon-start screen-texts

--- lunarlanderex.js
+++ lunarlanderex.js
@@ -1,18 +1,16 @@
 let x = 200;
 let y = 100;
-const speed = 10;
-let enemyX = 0;
-let enemyY = 0;
+let displayingStartScreen = true;
 let stars = [];
-let rocketY = 145;
-let isLaunching = false;
+let rocketY = 0;
 let velocity = 0;
 let crashed = 0;
-const gravity = 0.9;
+const gravity = 0.1;
 
+//background setup
 function setup() {
   createCanvas(700, 618);
-  for (let i = 0; i < 700; i++) {
+  for (let i = 0; i < 1000; i++) {
     const star = {
       x: Math.floor(Math.random() * width),
       y: Math.floor(Math.random() * height),
@@ -22,7 +20,24 @@ function setup() {
   }
 }
 
-function rocket(x, y, s, isLaunching) {
+//start screen display
+function startScreen() {
+  background(255, 182, 193);
+
+  textSize(30);
+  noStroke();
+  fill(128, 128, 128);
+  rect(230, 280, 190, 70, 100);
+  fill(0, 0, 0);
+  strokeWeight(2);
+  textFont("Courier New");
+  text("START", 280, 320);
+  if (mouseIsPressed) {
+    displayingStartScreen = false;
+  }
+}
+
+function rocket(x, y, s) {
   noStroke();
 
   //rocket
@@ -77,92 +92,92 @@ function rocket(x, y, s, isLaunching) {
     x + 123 * s,
     y + 462 * s
   );
-
-  //fire showing when launching
-
-  if (isLaunching === true) {
-    push();
-    fill(255, 0, 0);
-    stroke(255, 100, 0);
-    strokeWeight(3);
-    triangle(
-      x + 60 * s,
-      y + 487 * s,
-      x + 68 * s,
-      y + 502 * s,
-      x + 73 * s,
-      y + 487 * s
-    );
-    triangle(
-      x + 72 * s,
-      y + 487 * s,
-      x + 81 * s,
-      y + 502 * s,
-      x + 86 * s,
-      y + 487 * s
-    );
-    pop();
-  } else {
-    clear();
-  }
-
-  // moon surface
-  fill(128, 128, 128);
-  rect(0, 533, 700, 100);
 }
 
-//moving obstacle
-
-function enemy(x, y, s) {
-  fill(0, 110, 100);
+//fire showing when launching on press
+function fire(x, y, s) {
+  push();
+  fill(255, 0, 0);
+  stroke(255, 100, 0);
+  strokeWeight(3);
   triangle(
-    x + 0 * s,
-    y + 99 * s,
-    x + 0 * s,
-    y + 124 * s,
-    x + 29 * s,
-    y + 114 * s
+    x + 60 * s,
+    y + 487 * s + 2,
+    x + 68 * s,
+    y + 502 * s + 2,
+    x + 73 * s,
+    y + 487 * s + 2
   );
-
   triangle(
-    x + 0 * s,
-    y + 99 * s + 250,
-    x + 0 * s,
-    y + 124 * s + 250,
-    x + 29 * s,
-    y + 114 * s + 250
+    x + 72 * s,
+    y + 487 * s + 2,
+    x + 81 * s,
+    y + 502 * s + 2,
+    x + 86 * s,
+    y + 487 * s + 2
   );
-
-  if (enemyX > 660) {
-    enemyX = 0;
-  }
+  pop();
 }
 
 function draw() {
-  background(0, 0, 0);
-  rocket(x, rocketY, 0.8, true);
-  enemy(enemyX, enemyY, 1);
-  enemyX = enemyX + 5;
+  if (displayingStartScreen) {
+    startScreen();
+  } else {
+    background(0, 0, 0);
 
-  //star scenery
-  fill(255, 255, 255);
-  for (let star of stars) {
-    ellipse(star.x, star.y, 1);
+    //star scenery
+    fill(255, 255, 255);
+    for (let star of stars) {
+      ellipse(star.x, star.y, 1);
+    }
+
+
+    // moon surface
+    fill(128, 128, 128);
+    rect(0, 533, 700, 100);
+    rocket(x, rocketY, 0.4, true);
   }
 
-  //gravity when launching
+  // gravity when launching
 
-  rocketY += velocity;
-  if (keyIsDown(32)) {
-    velocity = -9;
-    isLaunching = true;
+  if (keyIsPressed) {
+    let fireX = x;
+    let fireY = rocketY;
+    fire(fireX, fireY, 0.4);
+    velocity = -3;
   } else {
     //falling when key released
-    velocity = velocity + gravity;
-    isLaunching = false;
+
+    velocity += gravity;
   }
 
-  if (rocketY > 533) {
-    console.log("crashed");
+  rocketY += velocity;
+  //landing win!
+  if (rocketY >= 339) {
+    rocketY = 339; // rocket on moon surface
+    velocity = 0;
+
+    console.log("Rocket has landed!");
+
+ console.log("Rocket has landed!");
+
+fill(255);
+textSize(47);
+    text ("Rocket has landed!",110,280);
+    
+
   }
+
+
+  //crashing lose !
+  /* else if (rocketY >= 339) {
+      rocketY = 339; // rocket on moon surface
+      velocity =3 ;
+      console.log("Rocket has crashed!");
+
+fill(255);
+textSize(47);
+    text ("Rocket has crashed!",110,280);
+
+  }*/
 }
